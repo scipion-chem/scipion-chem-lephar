4092234e7ddd96705277001c89ec837dfe40c894
DaniDelHoyo <danieldelhoyog@gmail.com>
2022-08-30 16:51:33 +0200

    Radius wizard adapted

--- lephar/wizards/wizard_radius.py
+++ lephar/wizards/wizard_radius.py
@@ -35,9 +35,10 @@ to select the radius of the sphere that contains the protein or a desired zone.
 from pwchem.wizards import GetRadiusProtein, SelectMultiChainWizard
 from ..protocols import *
 
-class GetRadiusProteinLePhar(GetRadiusProtein):
-    _targets = [(ProtChemLeDock, ['radius']),
-                ]
+GetRadiusProtein().addTarget(protocol=ProtChemLeDock,
+                             targets=['radius'],
+                             inputs=['inputAtomStruct'],
+                             outputs=['radius'])
 
 SelectMultiChainWizard().addTarget(protocol=ProtChemLePro,
                                    targets=['chain_name'],
