de97c2169640d7f15a85257aa3bac1def4b91e2b
Blanca Pueche <blancapueche@gmail.com>
2025-11-04 13:24:44 +0100

    remove outFile before creating link

--- lephar/protocols/protocol_ledock.py
+++ lephar/protocols/protocol_ledock.py
@@ -325,7 +325,7 @@ class ProtChemLeDock(EMProtocol):
 
     def linkLocal(self, sourcePath, outDir):
         outFile = os.path.join(outDir, os.path.basename(sourcePath))
-        if not os.path.exists(outFile):
+        if os.path.exists(outFile):
             os.remove(outFile)
         os.symlink(sourcePath, outFile)
         return os.path.basename(sourcePath)
